2502dba135747e584f12af93e223aca5a5b4f932
Aruzhan <190302188@stu.sdu.edu.kz>
2021-05-03 20:57:38 +0600

    03.05.2021

--- javascript.js
+++ javascript.js
@@ -1,23 +1,67 @@
-/*alert('This code is from another file')
-let n = 48; //Тип данных - число
-let b = "48"; //Тип данных - строка*/
 
-/*let age = prompt('Сколько тебе лет?', 100);
 
-alert(`Тебе ${age} лет!`); // Тебе 100 лет!
+/*let accessAllowed;
+let age = prompt('How old are you?', '');
 
-age = prompt('Сколько тебе лет?', 100);
+if (age>18) {
+    accessAllowed = true;
+} else {
+  accessAllowed = false;
+}
 
-alert("Тебе " + age + " лет!"); // Тебе 100 лет!*/
+alert(accessAllowed);*/
 
-/*let isBoss = confirm("Ты здесь главный?");
+/* let i = 0
+while (i<3) {
+  alert( i );
+  i++;
+} */
 
-alert( isBoss ); // true, если нажата OK*/
-
-let year = prompt('В каком году было ЭКСПО?', '');
+/*let year = prompt('В каком году было ЭКСПО?', '');
 
 if (year == 2017) {
-  alert( 'Вы правы!' );
+ alert( 'Вы правы!' );
+} else if {
+ alert( 'А вот и неправильно!' ); // любое значение, кроме 2017
+}*/
+/*
+let year = prompt ('В каком году был EXPO?')
+
+if (year < 2017) {
+  alert( 'Это слишком рано...' );
+} else if (year > 2017) {
+  alert( 'Это поздновато' );
 } else {
-  alert( 'А вот и неправильно!' ); // любое значение, кроме 2017
+  alert( 'Верно!' );
 }
+*/
+/*
+let year = prompt('В каком году был EXPO?');
+alert(year)
+/*
+while (year != 2017) {
+  year = prompt('В каком году был EXPO?', "");
+  if (year < 2017) {
+    alert( 'Это слишком рано...' );
+  } else if (year > 2017) {
+    alert( 'Это поздновато' );
+  } else {
+    alert( 'Верно!' );
+  }
+}*/
+
+
+/*function showMessage() {
+  alert('Hi everyone');
+}
+
+showMessage();
+showMessage();
+showMessage();*/
+
+/*function showMessage(from, text) { // аргументы: from, text
+  alert(from + ': ' + text);
+}
+
+showMessage('Astana', "+15"); // Аня: Привет! (*)
+*/
